b756cb339b179bf16490ad6824c6f3bc28e80b4e
sohanur28 <gsrahman28@gmail.com>
2025-07-05 16:53:45 +0600

    JSX, its rules and add dynamic content to JSX

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      
       <h1>Vite + React</h1>
-      
-      
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Student></Student>
     </>
-  )
+  );
 }
 
-export default App
+function Person() {
+  const age = 25;
+  const money = 20;
+  const person = { name: "Reyad", age: 12 };
+  return (
+    <h3>
+      I am {person.name} with age: {age + money}
+    </h3>
+  );
+}
+
+function Student() {
+  return (
+    <div>
+      <h3>This is a student</h3>
+      <p>Name:</p>
+      <p>age:</p>
+    </div>
+  );
+}
+
+export default App;
