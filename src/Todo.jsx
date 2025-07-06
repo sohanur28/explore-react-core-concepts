c34994ab9c79f0c1b3d17666ce762ee4a5e4047f
sohanur28 <gsrahman28@gmail.com>
2025-07-06 10:49:58 +0600

    If, Ternary and AND OR

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -14,11 +14,48 @@
 //   }
 // }
 
-export default function Todo({task, isDone}){
+// conditional rendering option 2
+// export default function Todo({task, isDone}){
 
+//     if(isDone){
+//         return <li>Finished: {task}</li>;
+//     }
+//     return <li>Work on: {task}</li>;
+
+// }
+
+// condititonal rendering option 3: ternary 
+// export default function Todo({task, isDone}){
+
+//     return (
+//         <li> {isDone ? 'Finished': 'Work on' } : {task}</li>
+//     )
+// }
+
+// conditional rendering option 4: &&
+// export default function Todo({task, isDone}){
+
+//     return (
+//         <li>{task} {isDone && ': Done'}</li>
+//     )
+// }
+
+// conditional rendering option 5: || (or)
+// export default function Todo({task, isDone}){
+
+//     return (
+//         <li>{task} {isDone || ': Do it'}</li>
+//     )
+// }
+
+// conditional rendering option 6: 
+export default function Todo({task, isDone}){
+    let listItem;
     if(isDone){
         return <li>Finished: {task}</li>;
     }
-    return <li>Work on: {task}</li>;
-
+    else{
+        listItem = <li>Work on: {task}</li>;
+    }
+    return listItem;
 }
